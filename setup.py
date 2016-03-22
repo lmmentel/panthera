37c5b065c17bbefe6b78ea6969c0352404161872
Lukasz Mentel <lmmentel@gmail.com>
2016-03-22 02:50:56 +0100

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,39 @@
+''' Thermopy setup script'''
+
+from setuptools.command.test import test as TestCommand
+import sys
+
+from setuptools import setup
+
+def readme():
+    '''Return the contents of the README.md file.'''
+    with open('README.md') as freadme:
+        return freadme.read()
+
+setup(
+    author = "Lukasz Mentel",
+    author_email = "lmmentel@gmail.com",
+    description = "Functions for calculating thermochemistry with anharmonic corrections",
+    include_package_data = True,
+    install_requires = [
+        'numpy >= 1.7',
+    ],
+    entry_points = {
+        'console_scripts' : [
+            'thermopy = thermo.themo:main',
+        ]
+    },
+    license = open('LICENSE.rst').read(),
+    long_description = readme(),
+    name = 'chemtools',
+    packages = ['thermo'],
+    url = 'https://bitbucket.org/lukaszmentel/thermopy/',
+    version = '0.1.0',
+    classifiers = [
+        'Environment :: Console',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+)
