cd97915f9739808fb2fa28fa28e38df4e45876cc
Lukasz Mentel <lmmentel@gmail.com>
2016-05-17 21:09:25 +0200

    added a function to perform the vibrational population analysis and another one to print the result adapted the calcualte_displacements function to accept a summetrized hessian in atomic units, read_vasp_hessian needs to be updated later

--- panther/displacements.py
+++ panther/displacements.py
@@ -3,9 +3,6 @@ from __future__ import print_function, absolute_import, division
 
 import numpy as np
 from scipy.constants import angstrom, pi, value
-from numpy.linalg import inv as npinvert
-
-import ase.io
 
 from writeBmat import get_internals
 
@@ -19,6 +16,11 @@ from .vibrations import project
 
 
 THR = 1.0e-6
+THRESH = 1.0e-14
+prm = 1.0 / value('electron mass in u')
+ang2bohr = angstrom / value('atomic unit of length')
+ev2hartree = value('electron volt-hartree relationship')
+invcm2au = 100.0 * value('inverse meter-hartree relationship')
 
 
 def calculate_displacements(atoms, hessian, npoints, mode_min=None,
@@ -28,7 +30,9 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
 
     Args:
         atoms : ase.Atoms
+            Atoms object
         hessian : numpy.array
+            Symmetric hessian matrix in atomic units [hartree/bohr**2]
         npoints : int
             Number of points to displace structure, the code will calculate
             ``2*npoints`` displacements since + and - directions are taken
@@ -37,10 +41,6 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
         mode_max : int
             Largest mode number
     '''
-
-    prm = 1.0 / value('electron mass in u')
-    ang2bohr = angstrom / value('atomic unit of length')
-    invcm2au = 100.0 * value('inverse meter-hartree relationship')
 
     natoms = atoms.get_number_of_atoms()
     ndof = 3 * natoms
@@ -56,11 +56,11 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
     coords = pos.ravel() * ang2bohr
 
     # write equilibrium POSCAR
-    ase.io.write('POSCAR.eq', atoms, vasp5=True)
+    #ase.io.write('POSCAR.eq', atoms, vasp5=True)
 
     # internals is a numpy record array with 'type' and 'value' records
     # bmatrix is a numpy array n_int x n_cart
-    internals, Bmatrix = get_internals(atoms, return_bmatrix=True)
+    internals, Bmatrix = get_internals(atoms, sort=False, return_bmatrix=True)
 
     # matrix with square root of masses
     B_mass_inv = np.zeros((ndof, ndof), dtype=float)
@@ -72,18 +72,20 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
 
     Gmatrix = np.dot(Bmatrix, np.dot(M_inv, Bmatrix.T))
 
-    Gmatrix_inv = npinvert(Gmatrix)
+    Gmatrix_inv = np.linalg.pinv(Gmatrix)
 
-    # symmetrize the hessian
-    hessian = (hessian + hessian.T) * 0.5e0
-
-    mwhessian = -np.dot(B_mass_inv, np.dot(hessian, B_mass_inv))
-
-    # TODO: projection needs to be updated for the case of mass weighted coords
-    #       see: projection.f90, trvmatr.f90 from eigenhess
-    prmwhessian = project(mwhessian)
-
+    # calculate hessian eigenvalues and eigenvectors
+    prhessian = project(atoms, hessian, ndof, proj_translations=True,
+                        proj_rotations=True)
+    prmwhessian = np.dot(B_mass_inv, np.dot(prhessian, B_mass_inv))
     evals, evecs = np.linalg.eigh(prmwhessian)
+
+    # sort eigenvalues and corresponding eiegenvectors in descending order
+    evals[evals < THRESH] = 0.0
+    vibdof = np.count_nonzero(evals)
+
+    evals = evals[::-1]
+    evecs = evecs[:, ::-1]
 
     mwevecs = np.dot(B_mass_inv, evecs)
 
@@ -91,15 +93,21 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
 
     Dmatrix = np.dot(Bmatrix, mwevecs)
 
-    is_stretch = vib_population(atoms, hessian, evecs, Bmatrix_inv, Dmatrix,
-                                internals)
+    eff_mass = 1.0 / np.diagonal(np.dot(mwevecs.T, mwevecs))
+
+    vibpop = vib_population(hessian, evals, Bmatrix_inv, Dmatrix, internals, vibdof)
+    is_stretch = vibpop['R'] > 0.9
+    
+    return internals, Bmatrix
+    #displ = np.zeros(len(internals), dtype=float)
+    #displ[is_stretch] = 8.0 / np.sqrt(2.0 * pi * np.sqrt(np.abs(evals[is_stretch])))
+    #displ[~is_stretch] = 4.0 / np.sqrt(2.0 * pi * np.sqrt(np.abs(evals[~is_stretch])))
+    #displ = displ / (npoints * 2.0)
 
     displ = np.zeros(len(internals), dtype=float)
     displ[is_stretch] = 8.0 / np.sqrt(2.0 * pi * np.sqrt(np.abs(evals[is_stretch])))
     displ[~is_stretch] = 4.0 / np.sqrt(2.0 * pi * np.sqrt(np.abs(evals[~is_stretch])))
     displ = displ / (npoints * 2.0)
-
-    eff_mass = 1.0 / np.diagonal(np.dot(mwevecs.T, mwevecs))
 
     for mode in range(mode_min, mode_max):
 
@@ -127,45 +135,88 @@ def calculate_displacements(atoms, hessian, npoints, mode_min=None,
                     not_converged = True
                     while not_converged:
                         # update atoms with new coords
-                        internals_new, _ = getbmatrix(atoms)
+                        internals_new = get_internals(atoms)
 
                         delta_int = internal_coord_disp - (internals_new - internals)
 
                         disp_norm = np.sqrt(np.dot(delta_int, delta_int))
 
 
-
-def vib_population(atoms, hessian, h_evecs, Bmatrix_inv, Dmatrix, internals, output):
+def vib_population(hessian, h_evals, Bmatrix_inv, Dmatrix, internals, vibdof,
+                   output='vib_pop.log'):
     '''
     Calculate the vibrational population analysis
 
     Args:
-        atoms : ase.Atoms
         hessian : numpy.array
+            Hessian matrix
+        h_evals : numpy.array
+            A vector of hessian eigenvalues
+        vibdof : int
+            Number of vibrational degrees of freedom
         output : str
+            Name of the file to store the results
 
     Returns:
-        is_stretch : numpy array
-            A boolean mask array, with ``True`` value for stretching frequencies
+        vibpop : numpy array
+            Numpy structured array with the vibrational populations for
+            stretches, bends, and torsions, per mode
     '''
+
+    nint, ndof = Dmatrix.shape
 
     # Wilson F matrix
     Fmatrix = np.dot(Bmatrix_inv.T, np.dot(hessian, Bmatrix_inv))
 
-    # norm of |Fmatrix - Fbcktr| can be used to check the accuracy of the transformation
-    # Fbcktr = np.dot(Bmatrix.T, np.dot(Fmatrix, Bmatrix))
+    # norm of |Fmatrix - Fbcktr| can be used to check the accuracy of
+    # the transformation Fbcktr = np.dot(Bmatrix.T, np.dot(Fmatrix, Bmatrix))
 
-    is_stretch = np.zeros(3 * atoms.get_number_of_atoms(), dtype=bool)
+    # construct the nu matrix with poppulation contributions from
+    # internal coordinates to cartesians
+    nu = np.multiply(Dmatrix.T, np.dot(Dmatrix.T, Fmatrix))
+    nu[nu < 0.0] = 0.0
+    # divide each column of nu by the hessian eigenvalues
+    nu = nu / h_evals[:, np.newaxis]
+
+    # sum over rows of nu to get the vibrational populations
+    internal_types = np.unique(internals['type']).tolist()
+    vibpop = np.zeros(ndof, dtype=list(zip(internal_types,
+                                [float] * len(internal_types))))
+
+    for inttype in internal_types:
+        mask = internals['type'] == inttype
+        vibpop[inttype] = np.sum(nu[:, mask], axis=1)
+
+    print_vib_pop(vibpop, h_evals, vibdof, output=output)
+
+    return vibpop
 
 
+def print_vib_pop(vibpop, evals, vibdof, output='vib_pop.log'):
+    '''
+    Print the vibrational population data
 
+    Args:
+        vibpop : numpy.recarray
+            Numpy structured array with the vibrational populations for
+            stretches, bends, and torsions, per mode
+        h_evals : numpy.array
+            A vector of hessian eigenvalues
+        vibdof : int
+            Number of vibrational degrees of freedom
+        output : str
+            Name of the file to store the printout, if ``None`` stdout will be
+            used
+    '''
 
+    if output is not None:
+        fobj = open(output, 'w')
 
-    # print a table in the format
-    #
-    # | mode  | omega [cm^-1] | stretch | bend | torsion | long-range |
-    # |------:|--------------:|--------:|-----:|--------:|-----------:|
-    # |     1 |   3136.932765 |     100 |    0 |       0 |          0 |
-    # ...
+    print('{0:^5s} {1:^10s} {2:^8s} {3:^8s} {4:^8s}'.format('mode', 'omega', 'stretch', 'bend', 'torsion'), file=fobj)
+    for mode, row in enumerate(vibpop[:vibdof]):
+        print('{0:5d} {1:>10.4f} {2:>8.2%} {3:>8.2%} {4:>8.2%}'.format(
+            mode + 1, np.sqrt(evals[mode]) / invcm2au, row['R'], row['A'],
+            row['T']), file=fobj)
 
-    return is_stretch
+    if output is not None:
+        fobj.close()
