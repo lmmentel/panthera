updated and corrected the function for reading the hessian from the vasprun.xml file and added a test case

panther/io.py
@@ -126,11 +126,14 @@ def read_vasp_hessian(outcar='OUTCAR', symmetrize=True, convert_to_au=True,
     ----------
     outcar : str
         Name of the VASP output, default is ``OUTCAR``
+
     symmetrize : bool
         If ``True`` the hessian will be symmetrized
+
     convert_to_au : bool
         If ``True`` convert the hessian to atomic units, in the other
         case hessian is returned in [eV/Angstrom**2]
+
     dof_labels : bool, default is False
         If ``True`` a list of labels corresponding to the degrees of
         freedom will also be returned
@@ -146,7 +149,7 @@ def read_vasp_hessian(outcar='OUTCAR', symmetrize=True, convert_to_au=True,
     .. note::
        By default VASP prints negative hessian so the elements should be
        multiplied by -1 to restore the original hessian, this is done by
-       default
+       default, hessian in the XML file is **NOT** symmetrized by default
 
     '''
 
@@ -188,8 +191,8 @@ def read_vasp_hessian(outcar='OUTCAR', symmetrize=True, convert_to_au=True,
         return -1.0 * hessian
 
 
-def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
-                          convert_to_au=True, stripmass=True):
+def read_vasp_hessian_xml(xml='vasprun.xml', convert_to_au=True,
+                          stripmass=True):
     '''
     Parse the hessian from the VASP ``vasprun.xml`` file into a numpy array
 
@@ -197,14 +200,15 @@ def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
     ----------
     xml : str
         Name of the VASP output, default is ``vasprun.xml``
-    symmetrize : bool
-        If ``True`` the hessian will be symmetrized
+
     convert_to_au : bool
         If ``True`` convert the hessian to atomic units, in the other
         case hessian is returned in [eV/Angstrom**2]
+
     dof_labels : bool, default is False
         If ``True`` a list of labels corresponding to the degrees of
         freedom will also be returned
+
     stripmass : bool
         If ``True`` use VASP default masses to transform hessian to
         non-mass-weighted form
@@ -220,7 +224,7 @@ def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
     .. note::
        By default VASP prints negative hessian so the elements should be
        multiplied by -1 to restore the original hessian, this is done by
-       default
+       default, hessian in the XML file is symmetrized by default
 
     '''
 
@@ -236,13 +240,10 @@ def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
     natoms = int(root.find('atominfo/atoms').text)
     dof = 3 * natoms
 
-    # intialize defaults assuming complete hessian calculation
-    index = np.repeat(np.arange(natoms, dtype=int), 3)
-    hess_size = dof
-
     constblock = root.find(
-            'structure[@name="initialpos"]/varray[@name="selective"]')
+        'structure[@name="initialpos"]/varray[@name="selective"]')
     if constblock is not None:
+        # partial hessian calculation
         selective = np.ones((natoms, 3), dtype=bool)
         for i, v in enumerate(constblock):
             for j, fixed in enumerate(v.text.split()):
@@ -250,6 +251,10 @@ def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
 
         index = np.flatnonzero(selective.ravel())
         hess_size = index.size
+    else:
+        # complete hessian calculation
+        index = np.repeat(np.arange(natoms, dtype=int), 3)
+        hess_size = dof
 
     hessian = np.zeros((hess_size, hess_size), dtype=float)
 
@@ -264,13 +269,10 @@ def read_vasp_hessian_xml(xml='vasprun.xml', symmetrize=True,
             vasp_mass[element[1].text.strip()] = float(element[2].text)
 
         vasp_massvec = np.zeros(hess_size, dtype=float)
-        for i, j in enumerate(index):
+        for i, j in enumerate(np.floor_divide(index, 3)):
             vasp_massvec[i] = vasp_mass[species[j]]
 
         hessian *= np.sqrt(np.outer(vasp_massvec, vasp_massvec))
-
-    if symmetrize:
-        hessian = (hessian + hessian.T) * 0.5
 
     if convert_to_au:
         hessian = hessian * Bohr**2 / Hartree

test/test_io.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from scipy.constants import angstrom, value
 
-from panther.io import read_vasp_hessian
+from panther.io import read_vasp_hessian, read_vasp_hessian_xml
 
 
 ang2bohr = angstrom / value('atomic unit of length')
@@ -56,6 +56,12 @@ def test_read_vasp_hessian_hcha():
 
 def test_read_vasp_hessian_xml():
 
+    vaspxml = os.path.join(cwd, 'data/hcha.vasprun.xml')
+    hessian = read_vasp_hessian_xml(vaspxml, convert_to_au=False,
+                                    stripmass=True)
+
     refhessian = np.load(os.path.join(cwd, 'data/hcha_hessian.npy'))
 
-    vaspxml = os.path.join(cwd, 'data/hcha.xml')
+    refhessian = (refhessian + refhessian.T) * -0.5
+
+    assert np.allclose(refhessian, hessian, atol=1.0e-6)