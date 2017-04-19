643893d31697303c3af68bd3fb4da8401cb856c1
Lukasz Mentel <lmmentel@gmail.com>
2017-04-19 14:42:14 +0200

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name = 'panther',
     packages = ['panther'],
     url = 'https://bitbucket.org/lukaszmentel/panther/',
-    version = '0.5.0',
+    version = '0.5.1',
     classifiers = [
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
