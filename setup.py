92ec06d1b581b3d93f43454bcd563c802fdfac9b
Lukasz Mentel <lmmentel@gmail.com>
2017-11-23 13:00:46 +0100

    removed obsolete whitespaces

--- setup.py
+++ setup.py
@@ -9,25 +9,26 @@ def readme():
     with open('README.md') as freadme:
         return freadme.read()
 
+
 setup(
-    author = "Lukasz Mentel",
-    author_email = "lmmentel@gmail.com",
-    description = "Package for calculating thermochemistry with anharmonic corrections",
-    include_package_data = True,
-    entry_points = {
-        'console_scripts' : [
+    author="Lukasz Mentel",
+    author_email="lmmentel@gmail.com",
+    description="Package for calculating thermochemistry with anharmonic corrections",
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
             'panther = panther.panther:main',
             'plotmode = panther.cli:plotmode_cli',
             'writemodes = panther.cli:write_modes_cli',
         ]
     },
-    license = open('LICENSE.rst').read(),
-    long_description = readme(),
-    name = 'panther',
-    packages = ['panther'],
-    url = 'https://bitbucket.org/lukaszmentel/panther/',
-    version = '0.5.1',
-    classifiers = [
+    license=open('LICENSE.rst').read(),
+    long_description=readme(),
+    name='panther',
+    packages=['panther'],
+    url='https://bitbucket.org/lukaszmentel/panther/',
+    version='0.5.1',
+    classifiers=[
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 2',
